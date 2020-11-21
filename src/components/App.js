679f13f51a55529e761467cf3c8f7b4f3bdde8a6
Alexandre Moreira <4lexandre.moreira@gmail.com>
2020-11-20 21:28:58 -0300

    AppInit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu primeiro App</h1>
+      <Navbar />
+    </div>
+  );
+
+}
+
+  export default App;
